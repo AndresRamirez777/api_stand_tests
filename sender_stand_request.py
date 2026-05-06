c45986414c53d916b51c63415cdabac875184da1
Juan Andres Ramirez <andresramirezgalvis6@gmail.com>
2026-05-05 19:40:01 -0500

    Mi primer commit

---
+++ sender_stand_request.py
@@ -0,0 +1,18 @@
+import requests
+import configuration
+import data
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+response = get_users_table()
+print(response.status_code)
+
